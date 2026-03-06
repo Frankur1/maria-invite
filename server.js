0cd205b2efbd6b44795b981f452fe073a6d5e2b5
Frankur1 <sargisbeybutyan@gmail.com>
2026-03-06 16:01:03 +0400

    fix

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get("/", (req,res)=>{
-res.sendFile(path.join(__dirname,"index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, ()=>{
-console.log("Server running on port",PORT);
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
